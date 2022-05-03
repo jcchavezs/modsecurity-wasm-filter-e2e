378499cd976bc8c3f903bcae50158a467dee0b4f
M4tteoP <pace.matteo96@gmail.com>
2022-05-03 16:14:53 +0000

    chore: fixing config and adding e2e script

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
 build-wasm-plugin:
+	cd modsecurity-wasm-filter/wasmplugin; sed -i 's/envoy-wasm-modsecurity-dynamic/envoy-wasm-modsecurity/' Dockerfile; sed -i 's/envoy-wasm-modsecurity-dynamic/envoy-wasm-modsecurity/' Makefile
 	cd modsecurity-wasm-filter/wasmplugin; docker build -t jcchavezs/modsecurity-wasm-filter:latest -f Dockerfile .
+	cd modsecurity-wasm-filter/wasmplugin; sed -i 's/envoy-wasm-modsecurity/envoy-wasm-modsecurity-dynamic/' Dockerfile; sed -i 's/envoy-wasm-modsecurity/envoy-wasm-modsecurity-dynamic/' Makefile
 
 extract-wasm-plugin:
 	docker rm -f modsecurity-wasm-filter-extract || true
 	@docker create --rm --name modsecurity-wasm-filter-extract jcchavezs/modsecurity-wasm-filter:latest /plugin.wasm
-	@mkdir ./bin
+	@mkdir -p ./bin
 	@docker cp modsecurity-wasm-filter-extract:/plugin.wasm ./bin/modsecurity-filter.wasm
 	@docker rm -f modsecurity-wasm-filter-extract
 
---
+++ tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+step=1
+total_steps=3
+application_url="http://localhost:8000"
+envoy_url_unfiltered="http://localhost:8001/home"
+envoy_url_filtered="http://localhost:8001/admin"
+
+# Testing if the server is up
+echo "[$step/$total_steps] Testing application reachability"
+status_code=$(curl --write-out "%{http_code}" --silent --output /dev/null $application_url)
+if [[ "$status_code" -ne 200 ]] ; then
+  echo "[Fail] Unexpected response with code $status_code from $application_url"
+  exit 1
+fi
+echo "[Ok] Got status code $status_code, expected 200"
+
+# Testing envoy container reachability with an unfiltered request
+((step+=1))
+echo "[$step/$total_steps] Testing true negative request"
+status_code=$(curl --write-out "%{http_code}" --silent --output /dev/null $envoy_url_unfiltered)
+if [[ "$status_code" -ne 200 ]] ; then
+  echo "[Fail] Unexpected response with code $status_code from $envoy_url_unfiltered"
+  exit 1
+fi
+echo "[Ok] Got status code $status_code, expected 200"
+
+# Testing filtered request
+((step+=1))
+echo "[$step/$total_steps] Testing true positive request"
+status_code=$(curl --write-out "%{http_code}" --silent --output /dev/null $envoy_url_filtered)
+if [[ "$status_code" -ne 403 ]] ; then
+  echo "[Fail] Unexpected response with code $status_code from $envoy_url_filtered"
+  exit 1
+fi
+echo "[Ok] Got status code $status_code, expected 403"
+
+echo "[Done] All tests passed"
